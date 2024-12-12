5a74348553811a7d940cd75d3edff3445ad34767
Ahmed Ehab <ahmedesrm@gmail.com>
2024-12-12 16:04:22 +0300

    Created basic server. Only prints out request

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp4", ":8080")
+	fmt.Println("Listening on 8080...")
+	if err != nil {
+		fmt.Printf("Error occured while listening. %v", err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("Error occured when Accepting. %v", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the incoming data
+	fmt.Printf("Received: %s", buf)
+}
