b50001234450f6564a58807e45c60324c0299360
AhmedEhab88 <ahmedesrm@gmail.com>
2024-12-13 20:33:47 +0300

    Reverted to using "net" package approach for it to work with Windows

--- main.go
+++ main.go
@@ -3,60 +3,34 @@ package main
 import (
 	"fmt"
 	"net"
-
-	"golang.org/x/sys/windows"
 )
 
 func main() {
-	socket, err := windows.Socket(windows.AF_INET, windows.SOCK_STREAM, windows.IPPROTO_TCP) // Create a new TCP Socket
-	if err != nil {
-		fmt.Printf("Error occured when creating socket %s", err)
-		return
-	}
-	sockAddr := windows.SockaddrInet4{Port: 8080, Addr: [4]byte{127, 0, 0, 1}}
-
-	err = windows.Bind(socket, &sockAddr) //Bind socket created to 127.0.0.1:8080
+	listener, err := net.Listen("tcp", ":8080")
 
 	if err != nil {
-		fmt.Println("ERROR while binding socket to localhost")
+		fmt.Println("Error while listening on port 8080")
 		return
 	}
 
-	//Listen on socket
-	err = windows.Listen(socket, 10) //Listening socket has backlog of 10 (Size of Accept Queue in Kernel)
+	defer listener.Close()
 
-	if err != nil {
-		fmt.Println("ERROR while listening on socket")
-		return
-	}
+	fmt.Println("Listening on 127.0.0.1:8080...")
 
-	fmt.Println("Listening on 127.0.0.1:8080")
-
-	//Accept incoming connection
 	for {
-		_, sa, err := windows.Accept(socket)
-
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("Error while accepting new connection: %v", err)
-			return
+			fmt.Printf("Error occured when Accepting. %v", err)
 		}
-
-		fmt.Printf("Address of connecting client: %s", sa)
+		go handleConnection(conn)
 	}
-
-	// for {
-	// 	conn, err := listener.Accept()
-	// 	if err != nil {
-	// 		fmt.Printf("Error occured when Accepting. %v", err)
-	// 	}
-	// 	go handleConnection(conn)
-	// }
 }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+
 	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
+	_, err := connection.Read(buf)
 	if err != nil {
 		fmt.Println(err)
 		return
